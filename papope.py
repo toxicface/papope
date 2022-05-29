eee712b6e57499f0a1d5c08d6bbf770d7a3afb9b
toxicface <47838537+toxicface@users.noreply.github.com>
2022-05-29 00:50:54 -0400

    Add files via upload

---
+++ papope.py
@@ -0,0 +1,30 @@
+# bot.py
+import os
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if 'im ' in message.content:
+        user = message.content
+        user = user.replace('im ', '')
+        response = 'Hi ' + user + ', I\'m Ian\'s test bot'
+        await message.channel.send(response)
+
+client.run(TOKEN)
