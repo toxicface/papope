eb0eba8e595916d220d5518de77ec1b83ded565c
toxicface <47838537+toxicface@users.noreply.github.com>
2022-05-29 01:07:21 -0400

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,4 @@
+# bot.py
 import os
 
 import discord
@@ -5,15 +6,14 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-
+GUILD = os.getenv('DISCORD_GUILD')
 client = discord.Client()
 
 @client.event
 async def on_ready():
     guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
     print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
+        "hi"
     )
 @client.event
 async def on_message(message):
